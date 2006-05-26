1ae43becaf69d14dabfbdd753b774e8f3eba1581
snookle <snookle@1ebc5623-af14-0410-b0a0-dfb113887855>
2006-05-26 13:39:24 +0000

    Stopped fav server list from showing private field.

--- trunk/frontend/MainForm.cs
+++ trunk/frontend/MainForm.cs
@@ -403,11 +403,15 @@ namespace LFS_ServerBrowser
 						ListViewItem lvi = l.Items.Add(info.host.ToString());
 						lvi.SubItems.Insert(0, new ListViewItem.ListViewSubItem(lvi, serverName));
 						lvi.SubItems.Insert(1, new ListViewItem.ListViewSubItem(lvi, info.ping.ToString()));
-						lvi.SubItems.Insert(2, new ListViewItem.ListViewSubItem(lvi, info.passworded == true ? "Yes" : "No"));
-						lvi.SubItems.Insert(3, new ListViewItem.ListViewSubItem(lvi, info.players.ToString() +"/" + info.slots.ToString()));
-						lvi.SubItems.Insert(4, new ListViewItem.ListViewSubItem(lvi, RulesToString(info.rules)));
-						lvi.SubItems.Insert(5, new ListViewItem.ListViewSubItem(lvi, info.track));
-						lvi.SubItems.Insert(6, new ListViewItem.ListViewSubItem(lvi, cars));
+						int columnOffset = 0;
+						if (isFavQuery){
+							columnOffset = 1;
+						} else
+							lvi.SubItems.Insert(2, new ListViewItem.ListViewSubItem(lvi, info.passworded == true ? "Yes" : "No"));
+						lvi.SubItems.Insert(3 - columnOffset, new ListViewItem.ListViewSubItem(lvi, info.players.ToString() +"/" + info.slots.ToString()));
+						lvi.SubItems.Insert(4 - columnOffset, new ListViewItem.ListViewSubItem(lvi, RulesToString(info.rules)));
+						lvi.SubItems.Insert(5 - columnOffset, new ListViewItem.ListViewSubItem(lvi, info.track));
+						lvi.SubItems.Insert(6 - columnOffset, new ListViewItem.ListViewSubItem(lvi, cars));
 					}
 			} else {
 					if (info.readFailed)
