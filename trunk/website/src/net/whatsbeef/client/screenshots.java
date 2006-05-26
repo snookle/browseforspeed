1bb93ac84912179e1f2dafaeefd79b7001d972c2
wabz <wabz@1ebc5623-af14-0410-b0a0-dfb113887855>
2006-05-26 12:16:59 +0000

    initial add of website

---
+++ trunk/website/src/net/whatsbeef/client/screenshots.java
@@ -0,0 +1,10 @@
+package net.whatsbeef.client;
+import java.util.ArrayList;
+import com.google.gwt.user.client.ui.*;
+
+public class screenshots {
+	public static HTML getHTML () {
+		String html = "<div class='page'><img src='img/screenshot.png' /></div>";
+		return new HTML(html);
+	}
+}
