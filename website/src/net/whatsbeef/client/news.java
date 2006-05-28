7688efd7f56eb0120f6794e7e793ed16897f6520
Richard Nelson <wabzqem@users.noreply.github.com>
2006-05-28 01:00:01 +0000

    updated for release

--- website/src/net/whatsbeef/client/news.java
+++ website/src/net/whatsbeef/client/news.java
@@ -4,7 +4,7 @@ import com.google.gwt.user.client.ui.*;
 
 public class news {
 	public static HTML getHTML () {
-		String html = "<div class='page'><H2>Browse For Speed 0.1 - Released (Not quite)!</H2><p><a href='/files/browseforspeed-0.1.zip'>Browse for Speed 0.1</a> - All you need to run it.</p>"
+		String html = "<div class='page'><H2>Browse For Speed 0.1 - Beta Release!</H2><p><a href='/files/browseforspeed-0.1.zip'>Browse for Speed 0.1</a> - All you need to run it.</p>"
 			+ "<p><a href='/files/browseforspeed-0.1-src.zip'>Browse for Speed 0.1 Source Code</a> - Only download this if you want to peruse the source, make changes and submit patches :)</p>"
 			+ "<p><a href='http://msdn.microsoft.com/netframework/downloads/updates/default.aspx'>.NET 2.0</a> - If you don't have this already, you will need it. Go there and download the 'Redistributable Package'.</p>"
 			+ "<p> The library was developed with <a href='http://www.monodevelop.com/Main_Page'>MonoDevelop</a>, and requires .NET 1.1. The front-end code requires .NET 2.0. This is the first project we have ever written in c#."
