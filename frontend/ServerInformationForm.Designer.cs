73bfeac79553b9defd5cc7ec11ca987d8d467318
Ben <ben.kenny@gmail.com>
2006-06-04 07:51:40 +0000

    Lots of changes: *Fixed friends missing bug - not sure if it's fixed well though. *Re-write list handling, added a number of subclasses of ListView which handles both our model and display lists at the same time. *ServerListView.Filter() needs implementing. *Moved initilisation code to Form.OnLoad()

--- frontend/ServerInformationForm.Designer.cs
+++ frontend/ServerInformationForm.Designer.cs
@@ -6,7 +6,7 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
-namespace LFS_ServerBrowser
+namespace BrowseForSpeed.Frontend
 {
 	partial class ServerInformationForm : System.Windows.Forms.Form
 	{
